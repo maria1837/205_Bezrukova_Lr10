c4bd9bf1b67cdd770aa8d759b4bb52564f80a443
maria1837 <134050927+maria1837@users.noreply.github.com>
2023-05-20 00:02:33 +0300

    Add files via upload

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,118 @@
+﻿using System;
+
+namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Label1 = new System.Windows.Forms.Button();
+            this.Label2 = new System.Windows.Forms.Button();
+            this.Label3 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Label1
+            // 
+            this.Label1.Location = new System.Drawing.Point(37, 206);
+            this.Label1.Name = "Label1";
+            this.Label1.Size = new System.Drawing.Size(130, 80);
+            this.Label1.TabIndex = 0;
+            this.Label1.Text = "Вид";
+            this.Label1.UseVisualStyleBackColor = true;
+            this.Label1.Click += new System.EventHandler(this.Label1_Click);
+            // 
+            // Label2
+            // 
+            this.Label2.Location = new System.Drawing.Point(211, 206);
+            this.Label2.Name = "Label2";
+            this.Label2.Size = new System.Drawing.Size(130, 80);
+            this.Label2.TabIndex = 1;
+            this.Label2.Text = "Форма";
+            this.Label2.UseVisualStyleBackColor = true;
+            this.Label2.Click += new System.EventHandler(this.Label2_Click);
+            // 
+            // Label3
+            // 
+            this.Label3.Location = new System.Drawing.Point(392, 206);
+            this.Label3.Name = "Label3";
+            this.Label3.Size = new System.Drawing.Size(130, 80);
+            this.Label3.TabIndex = 2;
+            this.Label3.Text = "Информация";
+            this.Label3.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(559, 284);
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(583, 308);
+            this.Controls.Add(this.Label3);
+            this.Controls.Add(this.Label2);
+            this.Controls.Add(this.Label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void Label2_Click(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void Label1_Click(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Label1;
+        private System.Windows.Forms.Button Label2;
+        private System.Windows.Forms.Button Label3;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public partial class ЛабораторнаяРабота4 : Form
+
+        {
+          
+            Rectangle Rectangle = new Rectangle(10, 10, 200, 100);
+
+            Rectangle Circle = new Rectangle(220, 10, 150, 150);
+
+            Rectangle Square = new Rectangle(380, 10, 150, 150);
+
+
+            private int LastClicked;
+            private bool RectangleClicked;
+            private int RectangleX;
+            private int RectangleY;
+            private object CircleX;
+            private object CircleY;
+            private object SquareX;
+            private object SquareY;
+
+            private void Label1_Click(object sender, EventArgs e, Button label1, Button label3)
+            {
+
+                if ((label1.Location.X < Square.X + Square.Width) && (label1.Location.X > Square.X))
+                {
+                    if ((label1.Location.Y < Square.Y + Square.Height) && (label1.Location.Y > Square.Y))
+                    {
+
+                        label3.Text = "Синий квадрат";
+
+                    }
+
+                }
+
+            }
+
+            private void Label2_Click(object sender, EventArgs e, Button label1, Button label2)
+            {
+
+                int X, Y, dX, dY;
+
+                int LastClicked = 0;
+
+                if (LastClicked == 2)
+
+                {
+
+                    if ((label2.Location.X < Circle.X + Circle.Width) && (label2.Location.X > Circle.X))
+
+                    {
+
+                        if ((label2.Location.Y < Circle.Y + Circle.Height) && (label2.Location.Y > Circle.Y))
+
+                        {
+
+                            X = Circle.X;
+
+                            Y = Circle.Y;
+
+                            dX = Circle.X;
+
+                            dY = Circle.Y;
+                            CircleX = SquareX;
+
+                            CircleY = SquareY;
+
+                            CircleX = SquareX;
+
+                            CircleY = SquareY;
+                            Square.X = X;
+
+                            Square.Y = Y;
+
+                            SquareX = dX;
+
+                            SquareY = dY;
+
+                        }
+
+                    }
+
+                }
+              
+            }
+            private void PictureBox1_Click(object sender, PaintEventArgs e, PictureBox pictureBox)
+
+            {
+
+                e.Graphics.FillEllipse(Brushes.Red, Circle);
+
+                e.Graphics.FillRectangle(Brushes.Blue, Square);
+
+                e.Graphics.FillRectangle(Brushes.Yellow, Rectangle);
+
+                int X = 0;
+                int Y = 0;
+
+
+                if ((X < Rectangle.X + Rectangle.Width) && (X > Rectangle.X))
+
+                {
+
+                    if ((Y < Rectangle.Y + Rectangle.Height) && (Y > Rectangle.Y))
+
+                    {
+
+                        RectangleClicked = true;
+                        RectangleX = X - Rectangle.X;
+
+                        RectangleY = Y - Rectangle.Y;
+                    }
+
+                }
+
+                pictureBox.Invalidate();
+            }
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+    }
+}
